6a458695d5ecacf4cefe40d692785943dacb3d6c
wismko <william@thewhisk.dev>
2021-06-09 20:33:45 +0100

    beam me up

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,5 @@
-const BlogList = (props) => {
-    const blogs = props.blogs;
-    const title = props.title;
+const BlogList = ({ blogs, title }) => {
+
     return (
         <div className="blogList">
             <h2> { title } </h2>
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum . . . ', author: 'mario', id: 1},
-        { title: 'Welcome party!', body: 'lorem impsum . . . ', author: 'yoshi', id: 2},
-        { title: 'web dev top tips', body: 'lorem ipsum . . .', author: 'mario', id: 3}
-    ]);
+    const [blogs, setBlogs] = useState(null);
+
+    const [name, setName] = useState('mario');
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
+    }, []);
 
     return (  
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs"/>
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
 }
